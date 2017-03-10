eac9ac519139a57c3f0589d9408f663c38b970b3
beini <874140704@qq.com>
2017-03-10 19:06:24 +0800

    add  zoom

--- app/src/main/java/com/example/administrator/baseapp/ui/fragment/camera/PicDetaiFragment.java
+++ app/src/main/java/com/example/administrator/baseapp/ui/fragment/camera/PicDetaiFragment.java
@@ -1,6 +1,8 @@
 package com.example.administrator.baseapp.ui.fragment.camera;
 
 
+import android.graphics.drawable.Drawable;
+
 import com.example.administrator.baseapp.R;
 import com.example.administrator.baseapp.base.BaseFragment;
 import com.example.administrator.baseapp.bind.ContentView;
@@ -9,6 +11,10 @@ import com.example.administrator.baseapp.ui.fragment.camera.bean.ImageBean;
 import com.facebook.drawee.view.SimpleDraweeView;
 import org.greenrobot.eventbus.EventBus;
 
+import java.net.URL;
+
+import uk.co.senab.photoview.PhotoView;
+
 /**
  * create by beini  2017/8/10
  */
@@ -16,10 +22,17 @@ import org.greenrobot.eventbus.EventBus;
 public class PicDetaiFragment extends BaseFragment {
     @ViewInject(R.id.picDetail_simpleView)
     SimpleDraweeView picDetail_simpleView;
+    @ViewInject(R.id.photo_view)
+    PhotoView photoView;
 
     @Override
     public void initView() {
-        picDetail_simpleView.setImageURI("file://" + EventBus.getDefault().getStickyEvent(ImageBean.class).getUrl());
+//        picDetail_simpleView.setImageURI("file://" + EventBus.getDefault().getStickyEvent(ImageBean.class).getUrl());
+//
+//        Drawable bitmap = getResources().getDrawable(R.mipmap.ic_launcher);
+//        photoView.setImageDrawable(bitmap);
+//        URL  url=new URL("file://" + EventBus.getDefault().getStickyEvent(ImageBean.class).getUrl())
+//        photoView.setImageURI();
     }
 
 }
