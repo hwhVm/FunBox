ce1597c8ebc803c85d74b95825283f49a2ee86df
beini <874140704@qq.com>
2017-03-15 15:17:47 +0800

    add DiskLruCache

--- app/src/main/java/com/example/administrator/baseapp/db/cache/FileUtil.java
+++ app/src/main/java/com/example/administrator/baseapp/db/cache/FileUtil.java
@@ -1,4 +1,4 @@
-package com.example.administrator.baseapp.db.sqllite;
+package com.example.administrator.baseapp.db.cache;
 
 import android.annotation.SuppressLint;
 import android.os.Environment;
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.channels.FileChannel;
 
 /**
@@ -84,5 +85,63 @@ public class FileUtil {
 			}
 		}
 	}
+	/**
+	 * 格式化单位
+	 *
+	 * @param size
+	 *
+	 * @return
+	 */
+	public static String getFormatSize(double size) {
+		double kiloByte = size / 1024;
+		if (kiloByte < 1) {
+//            return size + "Byte";
+			return "0K";
+		}
 
+		double megaByte = kiloByte / 1024;
+		if (megaByte < 1) {
+			BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+			return result1.setScale(2, BigDecimal.ROUND_HALF_UP)
+					.toPlainString() + "K";
+		}
+
+		double gigaByte = megaByte / 1024;
+		if (gigaByte < 1) {
+			BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+			return result2.setScale(2, BigDecimal.ROUND_HALF_UP)
+					.toPlainString() + "M";
+		}
+
+		double teraBytes = gigaByte / 1024;
+		if (teraBytes < 1) {
+			BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+			return result3.setScale(2, BigDecimal.ROUND_HALF_UP)
+					.toPlainString() + "GB";
+		}
+		BigDecimal result4 = new BigDecimal(teraBytes);
+		return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()
+				+ "TB";
+	}
+
+	// 获取文件大小
+	//Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
+	//Context.getExternalCacheDir() --> SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+	public long getFolderSize(File file) throws Exception {
+		long size = 0;
+		try {
+			File[] fileList = file.listFiles();
+			for (int i = 0; i < fileList.length; i++) {
+				// 如果下面还有文件
+				if (fileList[i].isDirectory()) {
+					size = size + getFolderSize(fileList[i]);
+				} else {
+					size = size + fileList[i].length();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return size;
+	}
 }
