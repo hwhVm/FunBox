03e437c015deb2e08034bb0c137fb899010219f8
beini <874140704@qq.com>
2017-05-08 11:32:44 +0800

    Avoid Repet ClickListener
    Signed-off-by: beini <874140704@qq.com>

---
+++ app/src/main/java/com/beini/utils/listener/AvoidRepetClickListener.java
@@ -0,0 +1,25 @@
+package com.beini.utils.listener;
+
+import android.view.View;
+
+import java.util.Calendar;
+
+/**
+ * Created by beini on 2017/5/8.
+ */
+public class AvoidRepetClickListener implements View.OnClickListener {
+    public static final int MIN_CLICK_DELAY_TIME = 1000;
+    private long lastClickTime = 0;
+
+    @Override
+    public void onClick(View view) {
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        if (currentTime - lastClickTime > MIN_CLICK_DELAY_TIME) {
+            lastClickTime = currentTime;
+            onAvoidRepetClick(view);
+        }
+    }
+
+    private void onAvoidRepetClick(View view) {
+    }
+}
